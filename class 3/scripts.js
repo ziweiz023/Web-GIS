0b15f7a1cd52977f04204a6e8984b0aacd607ebd
ziweiz023 <zz4980@nyu.edu>
2025-04-07 17:40:09 -0400

    added art svg to titl

--- class 3/scripts.js
+++ class 3/scripts.js
@@ -61,96 +61,26 @@ map.on('click', 'gallery-points', (event) => {
       });
 
 
-// const slider = $("#light-slider");
-
-// function getRandomColor() {
-//     // Generate a random hex color
-//     const letters = '0123456789ABCDEF';
-//     let color = '#';
-//     for (let i = 0; i < 6; i++) {
-//         color += letters[Math.floor(Math.random() * 16)];
-//         }
-//         return color;
-//     }
-      
-// slider.on("input", function () {
-//     const randomColor = getRandomColor();
-//     $(".grid-container h1").css("color", randomColor);
-//     $("#lightbulb").css("stroke", randomColor);
-// });
-  
-$(document).ready(function () {
-    var h1 = $(".grid-container h1");
-    var text = h1.text();
-    h1.empty();
-    $.each(text.split(''), function(index, letter) {
-      h1.append($("<span>").text(letter));
-    });
-  });
-
-
+// jQuery code to change the color of the SVG element
 const slider = $("#light-slider");
 
 function getRandomColor() {
-    // Generate a random hex color
     const letters = '0123456789ABCDEF';
     let color = '#';
     for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
+        color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
 }
-  
+
 slider.on("input", function () {
-    $(".grid-container h1 span").each(function() {
-      $(this).css("color", getRandomColor());
-    });
-    $("#lightbulb").css("stroke", getRandomColor());
+  const randomColor = getRandomColor();
+  $("#art").css({
+    stroke: randomColor,
+    fill: randomColor
+  });
 });
 
 
-// $(document).ready(function () {
-//     const slider = $("#light-slider");
 
-//     // Function to update the background and styles based on slider value
-//     function updateLighting(value) {
-//         const brightness = value / 100; // Normalize to 0.0 - 1.0
-
-//         // Calculate background color based on brightness
-//         const color = `rgb(${255 * brightness}, ${255 * brightness}, ${brightness * 200})`;
-
-//         // Update the background color
-//         $("body").css("background-color", color);
-
-//         // Update lightbulb and text color based on brightness
-//         let lightbulbColor, textColor;
-//         if (brightness < 0.3) {
-//             lightbulbColor = "white";
-//             textColor = "white";
-//         } else if (brightness < 0.7) {
-//             lightbulbColor = "black";
-//             textColor = "#DCDCDC";
-//         } else {
-//             lightbulbColor = "red";
-//             textColor = "black";
-//         }
-//         $("#lightbulb").css("stroke", lightbulbColor);
-//         $("h2").css("color", textColor);
-
-//         // Update slider thumb and track color
-//         slider.css({
-//             "background": `linear-gradient(to right, ${lightbulbColor} 0%, ${lightbulbColor} ${value}%, #ddd ${value}%, #ddd 100%)`
-//         });
-//     }
-
-//     // Initialize the lighting based on the slider's initial value
-//     const initialValue = slider.val(); // Get the initial slider value
-//     updateLighting(initialValue);
-
-//     // Update lighting dynamically as the slider value changes
-//     slider.on("input", function () {
-//         const value = $(this).val(); // Get the slider value
-//         updateLighting(value);
-//     });
-// });
 
